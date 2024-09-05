7966175303b26cdd9796f8452ed21a1c0d78d3e2
yassine <yassinechmarekh88@gmail.com>
2024-09-05 16:40:41 +0100

    finish about page

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,22 @@
-// change job title
-var typed = new Typed(".job", {
-  strings: ["front-end developer", "back-end developer", "freelancer"],
-  typeSpeed: 100,
-  backSpeed: 80,
-  loop: true
+// about navigation skills
+const navigation_skills = document.querySelectorAll('.skills_sec .navigation_skills ul li');
+const content_skills = document.querySelectorAll('.skills_sec .content_skills ul li');
+
+navigation_skills.forEach(item => {
+  item.addEventListener('click', function () {
+    const content = document.querySelector(this.dataset.target);
+
+    navigation_skills.forEach(i=> {
+      i.classList.remove('active');
+    });
+
+    content_skills.forEach(i=> {
+      i.classList.remove('active');
+    });
+
+    content.classList.add('active');
+    this.classList.add('active');
+  });
 });
+
+
---
+++ assets/js/typed.js
@@ -0,0 +1,7 @@
+// change job title
+var typed = new Typed(".job", {
+    strings: ["front-end developer", "back-end developer", "freelancer"],
+    typeSpeed: 100,
+    backSpeed: 80,
+    loop: true
+  });
