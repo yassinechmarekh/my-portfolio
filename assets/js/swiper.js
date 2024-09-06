9e62eb1db132a36b3a616e8d8d33d6a2c82eeec5
yassine <yassinechmarekh88@gmail.com>
2024-09-06 03:04:22 +0100

    add services page

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -12,3 +12,34 @@ var swiper = new Swiper(".projects-swp", {
     // },
     loop: true,
   });
+
+
+// services swiper
+var swiper = new Swiper(".services-swp", {
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullests: true,
+      clickable: true,
+    },
+    slidesPerView: 2,
+    // autoplay: {
+    //     delay: 3500,
+    // },
+    loop: true,
+    breakpoints: {
+      950: {
+        slidesPerView: 2,
+      },
+      750: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+      500: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      0: {
+        slidesPerView: 1,
+      },
+    },
+  });
